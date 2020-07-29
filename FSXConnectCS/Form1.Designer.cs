5437f107a62c312d8fe6863d9f5efcf974dade47
tfgui <tfgui2@gmail.com>
2020-07-29 16:02:46 +0900

    version 1

---
+++ FSXConnectCS/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace FSXConnectCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(0, 244);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(800, 206);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(130, 55);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "FSXConnectCS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ FSXConnectCS/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.FlightSimulator.SimConnect;
+using System.Runtime.InteropServices;
+
+using System.Net;
+using System.Net.Sockets;
+
+using System.IO;
+using System.IO.Ports;
+
+
+/*
+enum CLIENT_EVENTS
+{
+    PITOT_TOGGLE,
+    FLAPS_INCR,
+    FLAPS_DECR,
+    FLAPS_UP,
+    FLAPS_DOWN,
+};
+*/
+namespace FSXConnectCS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Log("Welcome FSXConnect CS");
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            this.Init();
+        }
+
+        private void Log(string msg)
+        {
+            textBox1.Text += msg;
+            textBox1.Text += Environment.NewLine;
+            
+            textBox1.Select(textBox1.Text.Length, 0);
+            textBox1.ScrollToCaret();
+        }
+
+        // User-defined win32 event
+        const int WM_USER_SIMCONNECT = 0x0402;
+        // SimConnect object
+        SimConnect simconnect = null;
+        
+        protected override void DefWndProc(ref Message m)
+        {
+            if (m.Msg == WM_USER_SIMCONNECT)
+            {
+                if (simconnect != null)
+                    simconnect.ReceiveMessage();
+            }
+            else
+            {
+                base.DefWndProc(ref m);
+            }
+        }
+
+        // UDP property
+        static int udpPort = 1234;
+        UdpClient udpClient = null;
+        private string udpMsg;
+        IPEndPoint remoteEp = null;
+
+        private void Init()
+        {
+            // simconnect
+            if (simconnect == null)
+            {
+                try
+                {
+                    simconnect = new SimConnect("FSXConnectCS", this.Handle, WM_USER_SIMCONNECT, null, 0);
+                    this.InitClientEvent();
+
+                }
+                catch (COMException ex)
+                {
+                    this.Log("Unable to connect to FSX " + ex.Message);
+                }
+            }
+
+            // udp open
+            if (udpClient == null)
+            {
+                try
+                {
+                    IPEndPoint hostEp = GetHostEndPoint();
+                    udpClient = new UdpClient(hostEp);
+                    string addr = udpClient.Client.LocalEndPoint.ToString();
+                    this.Log("Udp Host : " + addr);
+                    backgroundWorker1.RunWorkerAsync();
+                }
+                catch(Exception e)
+                {
+                    this.Log(e.Message);
+                }
+            }
+
+            // serial open
+            if (serialPort1.IsOpen == false)
+            {
+                try
+                {
+                    serialPort1.PortName = GetSerialPortName();
+                    serialPort1.Open();
+                    this.Log("Serial open " + serialPort1.PortName);
+                }
+                catch(Exception e)
+                {
+                    this.Log(e.Message);
+                }
+            }
+
+        }
+
+        private IPEndPoint GetHostEndPoint()
+        {
+            UdpClient u = new UdpClient();
+            u.Connect("www.contoso.com", 11000);
+            IPEndPoint ep = (IPEndPoint)(u.Client.LocalEndPoint);
+            ep.Port = udpPort;
+            u.Close();
+            return ep;
+        }
+
+        private string GetSerialPortName()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            if (ports.Length > 0)
+                return ports[0];
+
+            return null;
+        }
+
+        // SimConnect Events
+        // clientEvent : any custom id, for this app. Enum
+        // simEvent : FSX declared event, string type
+        
+        enum NOTIFICATION_GROUPS
+        {
+            GROUP0,
+        }
+
+        private void addSimEvent(CLIENT_EVENTS clientEvent)
+        {
+            string simEvent = clientEvent.ToString();
+            simconnect.MapClientEventToSimEvent(clientEvent, simEvent);
+            simconnect.AddClientEventToNotificationGroup(NOTIFICATION_GROUPS.GROUP0, clientEvent, false);
+        }
+        private void sendSimEvent(CLIENT_EVENTS clientEvent)
+        {
+            simconnect.TransmitClientEvent(0, clientEvent, 0, NOTIFICATION_GROUPS.GROUP0, SIMCONNECT_EVENT_FLAG.GROUPID_IS_PRIORITY);
+        }
+        private void InitClientEvent()
+        {
+            try
+            {
+                // listen to connect and quit msgs
+                simconnect.OnRecvOpen += new SimConnect.RecvOpenEventHandler(simconnect_OnRecvOpen);
+                simconnect.OnRecvQuit += new SimConnect.RecvQuitEventHandler(simconnect_OnRecvQuit);
+                // listen to exceptions
+                simconnect.OnRecvException += new SimConnect.RecvExceptionEventHandler(simconnect_OnRecvException);
+                // listen to events
+                simconnect.OnRecvEvent += new SimConnect.RecvEventEventHandler(simconnect_OnRecvEvent);
+                // set the group priority
+                simconnect.SetNotificationGroupPriority(NOTIFICATION_GROUPS.GROUP0, SimConnect.SIMCONNECT_GROUP_PRIORITY_HIGHEST);
+
+                // add event
+                foreach (CLIENT_EVENTS ce in Enum.GetValues(typeof(CLIENT_EVENTS)))
+                {
+                    addSimEvent(ce);
+                }
+
+            }
+            catch (COMException ex)
+            {
+                this.Log(ex.Message);
+            }
+        }
+
+        void simconnect_OnRecvOpen(SimConnect sender, SIMCONNECT_RECV_OPEN data)
+        {
+            this.Log("Connected to FSX");
+        }
+
+        // The case where the user closes FSX
+        void simconnect_OnRecvQuit(SimConnect sender, SIMCONNECT_RECV data)
+        {
+            this.Log("FSX has exited");
+            if (simconnect != null)
+            {
+                // Dispose serves the same purpose as SimConnect_Close()
+                simconnect.Dispose();
+                simconnect = null;
+                this.Log("Connection closed");
+            }
+        }
+
+        void simconnect_OnRecvException(SimConnect sender, SIMCONNECT_RECV_EXCEPTION data)
+        {
+            this.Log("Exception received: " + data.dwException);
+        }
+
+        void simconnect_OnRecvEvent(SimConnect sender, SIMCONNECT_RECV_EVENT recEvent)
+        {
+            switch (recEvent.uEventID)
+            {
+                case (uint)CLIENT_EVENTS.COM_RADIO_WHOLE_DEC:
+
+                    this.Log("COM_RADIO_WHOLE_DEC");
+                    break;
+
+            }
+        }
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Any, udpPort);
+                byte[] buffer = udpClient.Receive(ref ep);
+                remoteEp = ep;
+                udpMsg = Encoding.UTF8.GetString(buffer);
+
+                this.Invoke(new EventHandler(udpReceived));
+            }
+        }
+
+        private void udpReceived(object sender, EventArgs e)
+        {
+            this.Log(udpMsg);
+        }
+
+        private void udpSend(string msg)
+        {
+            if (remoteEp == null)
+                return;
+
+            byte[] buffer = Encoding.UTF8.GetBytes(msg);
+            udpClient.Send(buffer, buffer.Length, remoteEp);
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            this.Invoke(new EventHandler(serialReceived));
+        }
+
+        private void serialReceived(object sender, EventArgs e)
+        {
+            int b = serialPort1.ReadByte();
+            if (Enum.IsDefined(typeof(CLIENT_EVENTS), b))
+            {
+                this.Log(b.ToString());
+                sendSimEvent((CLIENT_EVENTS)b);
+            }
+        }
+
+        private void serialSend(string msg)
+        {
+            if (serialPort1.IsOpen == false)
+            {
+                return;
+            }
+
+            serialPort1.Write(msg);
+        }
+
+    }
+}
